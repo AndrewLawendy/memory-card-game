87b093e47976ee783e7a7fd40d305fa3e2e4cabf
Andrew-Lawendy <andrew.lawendy@outlook.com>
2021-01-11 16:57:16 +0200

    🍱 Add more card images

--- utils/allCards.js
+++ utils/allCards.js
@@ -1,42 +1,29 @@
 const cardImages = require("../assets/images/cards/*.png");
 
 const allCards = [
-  {
-    id: "2dfa87ag91cf7",
-    imageSrc: cardImages[1],
-  },
-  {
-    id: "jj30251fgbg36",
-    imageSrc: cardImages[2],
-  },
-  {
-    id: "fej3i2cd71ah8",
-    imageSrc: cardImages[3],
-  },
-  {
-    id: "c1bdii699a1g5",
-    imageSrc: cardImages[4],
-  },
-  {
-    id: "59jj1d6e1j7h9",
-    imageSrc: cardImages[5],
-  },
-  {
-    id: "88c51cfjhje37",
-    imageSrc: cardImages[6],
-  },
-  {
-    id: "9jaage267a9dc",
-    imageSrc: cardImages[7],
-  },
-  {
-    id: "9ihjc8203b478",
-    imageSrc: cardImages[8],
-  },
-  {
-    id: "jbg1bgc30250a",
-    imageSrc: cardImages[9],
-  },
+  { id: "d687hj5957126", imageSrc: cardImages[1] },
+  { id: "h7ffhe6gfe81c", imageSrc: cardImages[2] },
+  { id: "5c983b693395", imageSrc: cardImages[3] },
+  { id: "53ebhehc2g13", imageSrc: cardImages[4] },
+  { id: "a5jah0jaj2di", imageSrc: cardImages[5] },
+  { id: "6a6h1c53060bb", imageSrc: cardImages[6] },
+  { id: "jabd3499d1bb9", imageSrc: cardImages[7] },
+  { id: "dbge06ff80059", imageSrc: cardImages[8] },
+  { id: "g5h26115g2537", imageSrc: cardImages[9] },
+  { id: "9cdjahg423he8", imageSrc: cardImages[10] },
+  { id: "cja5ih8a034g", imageSrc: cardImages[11] },
+  { id: "1i37771jbc904", imageSrc: cardImages[12] },
+  { id: "814adb2a2hbh", imageSrc: cardImages[13] },
+  { id: "d3h7bb1dh0d2", imageSrc: cardImages[14] },
+  { id: "33c1f3da8de8", imageSrc: cardImages[15] },
+  { id: "biiihch4773d", imageSrc: cardImages[16] },
+  { id: "18d4e15hfgj5e", imageSrc: cardImages[17] },
+  { id: "9g3bd3jdac4a", imageSrc: cardImages[18] },
+  { id: "ciaic563c093", imageSrc: cardImages[19] },
+  { id: "7c6ji6gc654ci", imageSrc: cardImages[20] },
+  { id: "ib26ahb7e3359", imageSrc: cardImages[21] },
+  { id: "h80b725489cib", imageSrc: cardImages[22] },
+  { id: "65a048e708b75", imageSrc: cardImages[23] },
+  { id: "39972hf0a97h2", imageSrc: cardImages[24] },
 ];
-
 export default allCards;
