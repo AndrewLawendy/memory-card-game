81ffee7480e21e75164c31b2c9589cfe877848c9
Andrew-Lawendy <andrew.lawendy@outlook.com>
2021-01-12 13:48:55 +0200

    🎨 Silly animation on game state change

--- src/components/GameNav/GameNav.jsx
+++ src/components/GameNav/GameNav.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { Transition } from "semantic-ui-react";
 
 import styles from "./styles.scss";
 
@@ -6,12 +7,19 @@ import { AppContext } from "../AppContext/AppContext.js";
 import GameTimer from "../GameTimer/GameTimer.jsx";
 
 const GameNav = () => {
-  const { moveCounts } = useContext(AppContext);
+  const { moveCounts, gameState } = useContext(AppContext);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(!animate);
+  }, [gameState]);
 
   return (
     <div className={styles.nav}>
       <h2 className="title">Moves {moveCounts}</h2>
-      <h1 className="title">Memory Card</h1>
+      <Transition animation="jiggle" duration={1500} visible={animate}>
+        <h1 className="title">Memory Card</h1>
+      </Transition>
       <GameTimer />
     </div>
   );
